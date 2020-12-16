9f53b259113bffe4993d36bb30f23ca8ab107859
KoljanTestwamore <anton.pab@gmail.com>
2020-12-16 17:55:16 +0300

    Implemented main logic on server

--- server.py
+++ server.py
@@ -3,12 +3,31 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from threading import Timer
 from constants import *
+import json
 import uuid
 
 Players = {}
+PlayersEvents = {}
+boxes = []
 
 def handleUpdate(data):
-    0
+    id = data["id"]
+    angle = data["angle"]
+    updater_player = Players[id]
+    # рассчитать действия игрока и отослать результаты
+    updater_player.SetX(data["x"] if data["x"] else updater_player.X())
+    updater_player.SetY(data["y"] if data["y"] else updater_player.Y())
+    updater_player.SetAngleView(data["angle"] if data["angle"] else updater_player.angle_view())
+
+    if (data["shot"] == True):
+        distance, enemy = hit(updater_player, Players, angle)
+        if (enemy):
+            if (distance < nearest_wall_shot(([updater_player.X(), updater_player.Y()],
+                                             [enemy.X(), enemy.Y()]), boxes)):
+                enemy.Decrease_HP(20)
+                PlayersEvents[enemy.ID()].append({
+                    "shot": True
+                })
 
 active_users = set()
 
@@ -27,6 +46,7 @@ def generate_new_player(data):
         data["x"] if data["x"] else 0, 
         data["y"] if data["y"] else 0,
         INITIAL_HP)
+    PlayersEvents[uid] = []
     active_users.add(uid)
     emit('uid', {
         "uid": uid
@@ -40,11 +60,22 @@ def test_message(message):
     })
 
 @socketio.on('update')
-def update(message):
-    handleUpdate(message)
-    if message.data:
-        print(str(message.data))
-    emit('update', message)
+def update(data):
+    handleUpdate(data)
+    if data:
+        print(str(data))
+    result = {
+        "players": []
+    }
+    for player in Players:
+        result["players"].append({
+            "x": player.X(),
+            "y": player.Y(),
+            "angle": player.angle_view(),
+            "hp": player.HP()
+        })
+
+    emit('update', result)
 
 if __name__ == '__main__':
     print("Running server! Visit http://127.0.0.1:5000/ for api")
